5621343971392ae3e242c39cb252538ab5421d6b
Wladimir J. van der Laan <laanwj@gmail.com>
2016-10-13 16:19:20 +0200

    Kill insecure_random and associated global state
    There are only a few uses of `insecure_random` outside the tests.
    This PR replaces uses of insecure_random (and its accompanying global
    state) in the core code with an FastRandomContext that is automatically
    seeded on creation.
    
    This is meant to be used for inner loops. The FastRandomContext
    can be in the outer scope, or the class itself, then rand32() is used
    inside the loop. Useful e.g. for pushing addresses in CNode or the fee
    rounding, or randomization for coin selection.
    
    As a context is created per purpose, thus it gets rid of
    cross-thread unprotected shared usage of a single set of globals, this
    should also get rid of the potential race conditions.
    
    - I'd say TxMempool::check is not called enough to warrant using a special
      fast random context, this is switched to GetRand() (open for
      discussion...)
    
    - The use of `insecure_rand` in ConnectThroughProxy has been replaced by
      an atomic integer counter. The only goal here is to have a different
      credentials pair for each connection to go on a different Tor circuit,
      it does not need to be random nor unpredictable.
    
    - To avoid having a FastRandomContext on every CNode, the context is
      passed into PushAddress as appropriate.
    
    There remains an insecure_random for test usage in `test_random.h`.

--- src/main.cpp
+++ src/main.cpp
@@ -5892,7 +5892,6 @@ bool static AlreadyHave(const CInv& inv)
     return true;
 }
 
-
 void static ProcessGetData(CNode* pfrom)
 {
     std::deque<CInv>::iterator it = pfrom->vRecvGetData.begin();
@@ -6250,13 +6249,14 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
             // Advertise our address
             if (fListen && !IsInitialBlockDownload()) {
                 CAddress addr = GetLocalAddress(&pfrom->addr);
+                FastRandomContext insecure_rand;
                 if (addr.IsRoutable()) {
                     LogPrintf("ProcessMessages: advertizing address %s\n", addr.ToString());
-                    pfrom->PushAddress(addr);
+                    pfrom->PushAddress(addr, insecure_rand);
                 } else if (IsPeerAddrLocalGood(pfrom)) {
                     addr.SetIP(pfrom->addrLocal);
                     LogPrintf("ProcessMessages: advertizing address %s\n", addr.ToString());
-                    pfrom->PushAddress(addr);
+                    pfrom->PushAddress(addr, insecure_rand);
                 }
             }
 
@@ -6363,8 +6363,9 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
                         mapMix.insert(make_pair(hashKey, pnode));
                     }
                     int nRelayNodes = fReachable ? 2 : 1; // limited relaying of addresses outside our network(s)
+                    FastRandomContext insecure_rand;
                     for (multimap<uint256, CNode*>::iterator mi = mapMix.begin(); mi != mapMix.end() && nRelayNodes-- > 0; ++mi)
-                        ((*mi).second)->PushAddress(addr);
+                        ((*mi).second)->PushAddress(addr, insecure_rand);
                 }
             }
             // Do not store addresses outside our network
@@ -6840,8 +6841,9 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
     else if ((strCommand == "getaddr") && (pfrom->fInbound)) {
         pfrom->vAddrToSend.clear();
         vector<CAddress> vAddr = addrman.GetAddr();
+        FastRandomContext insecure_rand;
         for (const CAddress& addr : vAddr)
-            pfrom->PushAddress(addr);
+            pfrom->PushAddress(addr, insecure_rand);
     }
 
 
