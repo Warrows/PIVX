1f623def923c9b8f43b9113be7d2d7afdff0cf35
warrows <warrows@gamer666.fr>
2018-05-28 11:44:47 +0200

    [Crypto] Improve randomness for key generation
    This uses OS randomness in addition to openssl's to generate a new
    keypair.
    It also removes the dependancy on openssl in memory cleanse

--- src/init.cpp
+++ src/init.cpp
@@ -1600,9 +1600,6 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
         }
 
         if (fFirstRun) {
-            // Create new keyUser and set as default key
-            RandAddSeedPerfmon();
-
             CPubKey newDefaultKey;
             if (pwalletMain->GetKeyFromPool(newDefaultKey)) {
                 pwalletMain->SetDefaultKey(newDefaultKey);
@@ -1878,8 +1875,6 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
 
     if (!strErrors.str().empty())
         return InitError(strErrors.str());
-
-    RandAddSeedPerfmon();
 
     //// debug print
     LogPrintf("mapBlockIndex.size() = %u\n", mapBlockIndex.size());
