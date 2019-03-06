964e17b13900bc2873a4ca280d78994ce6746c17
warrows <warrows@gamer666.fr>
2019-03-06 20:45:45 +0100

    [Refactor] Remove useless BOOST includes and readd where needed

--- src/walletdb.cpp
+++ src/walletdb.cpp
@@ -18,7 +18,6 @@
 #include <primitives/deterministicmint.h>
 
 #include <boost/filesystem.hpp>
-#include <boost/foreach.hpp>
 #include <boost/scoped_ptr.hpp>
 #include <boost/thread.hpp>
 #include <fstream>
