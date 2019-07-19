4e8d0f3a0540a2faf0451f391f6b1da59808f92a
warrows <warrows@gamer666.fr>
2019-07-19 15:57:41 +0200

    [Wallet] Do not flush the wallet in AddToWalletIfInvolvingMe(..)
    Backport of https://github.com/bitcoin/bitcoin/pull/4805 ( commit
    44bc988e7becb492a78ed92ea1052f4789012534 )

--- src/main.cpp
+++ src/main.cpp
@@ -3111,7 +3111,7 @@ bool UpdateZPIVSupply(const CBlock& block, CBlockIndex* pindex, bool fJustCheck)
                             CWalletTx wtx(pwalletMain, tx);
                             wtx.nTimeReceived = block.GetBlockTime();
                             wtx.SetMerkleBranch(block);
-                            pwalletMain->AddToWallet(wtx);
+                            pwalletMain->AddToWallet(wtx, false, nullptr);
                             setAddedToWallet.insert(txid);
                         }
                     }
@@ -3461,7 +3461,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
                         CWalletTx wtx(pwalletMain, tx);
                         wtx.nTimeReceived = pindex->GetBlockTime();
                         wtx.SetMerkleBranch(block);
-                        pwalletMain->AddToWallet(wtx);
+                        pwalletMain->AddToWallet(wtx, false, nullptr);
                         setAddedTx.insert(pSpend.second);
                     }
                 }
